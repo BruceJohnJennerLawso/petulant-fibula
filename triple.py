86c61871f87cb9632c9d43a98deb01e05f4c5711
bruce <johnnybgoode@rogers.com>
2015-11-08 11:39:02 -0500

    seems to work, but I could be wrong

--- triple.py
+++ triple.py
@@ -23,15 +23,35 @@ def rxBound(x):
 	output = x
 	output *= math.exp(-(x**3))
 	return output
-
+	
+def integral(intervalSize, xMax):
+	
+	output = 0
+	
+	xRange = np.arange(0, xMax, intervalSize)
+	
+	for cy in xRange:
+		print "%f, (y^2 + z^2) <= %f" % (cy, rxBound(cy))
+		
+	for xSlice in xRange:
+		radius = rxBound(xSlice)
+		yRange = np.arange(-radius, radius, intervalSize)
+		for ySlice in yRange:
+			if (abs(ySlice) != radius):
+				## the conditional is needed here so that we dont try to
+				## evaluate the edge of the circle, where our zrange will be
+				## empty
+				zSpan = math.sqrt(radius**2 - ySlice**2)
+				zRange = np.arange(-zSpan, zSpan, intervalSize)
+				for zSlice in zRange:
+					output += (rx(zSlice, ySlice)*(1 + ySlice + zSlice**2))
+	return output
+			## how do we set up our z range...?
+			
 
 if(__name__ == "__main__"):
 	xMax = inputFloat("Input upper bound for x: ")
 	
-	stepSize = 0.01
-	
-	xRange = np.arange(0, xMax, stepSize)
-	
-	for cy in xRange:
-		print "%f, (y^2 + z^2) <= %f" % (cy, rxBound(cy))
+	intervalSize = 0.01
+	print "Moment integral around x axis: %f" % integral(intervalSize, xMax)
 		
